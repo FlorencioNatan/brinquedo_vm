84af4d0c6a9a2e1f9d035857b0a3435304112f46
Florencio Natan <florencionatan@gmail.com>
2024-05-07 15:06:24 -0300

    Adiciona as instruções aritméticas para inteiros sem sinal

--- src/main.c
+++ src/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define X_7_0 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+#define X_6_0 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+#define X_5_0 0x00, 0x00, 0x00, 0x00, 0x00
+#define X_4_0 0x00, 0x00, 0x00, 0x00
+#define TESTE_PILHA 0x1F, 0x00, 0xFF, 0xEE, 0xDD, 0x20, 0xCC, 0x88
+
+
 #define MAX_TAM_PILHA 64
 #define MAX_TAM_PROGRAMA 64 * 8
 #define MAX_TAM_MEMORIA 64 * 8
 
-#define INST_NOP 0x00
+#define INST_NOP  0x00
 #define INST_HALT 0x01
 #define INST_PUSH 0x02
-#define INST_POP 0x03
+#define INST_POP  0x03
+
+#define INST_ADDU 0x04
+#define INST_SUBU 0x05
+#define INST_MULU 0x06
+#define INST_DIVU 0x07
+#define INST_REMU 0x08
 
 typedef struct bvm {
     uint64_t pc;
@@ -26,6 +39,8 @@ typedef struct bvm {
 void processar_instrucoes(bvm *vm) {
     uint8_t inst = vm->programa[vm->pc++];
     uint64_t imediato = 0;
+    uint64_t operando1;
+    uint64_t operando2;
 
     printf("Inst: 0x%02x\n", inst);
     switch (inst) {
@@ -38,17 +53,44 @@ void processar_instrucoes(bvm *vm) {
             for (uint64_t i = 8; i <= 64;i += 8) {
                 imediato |= ((uint64_t) vm->programa[vm->pc++]) << (64 - i);
             }
-            vm->pilha[vm->tam_pilha] = imediato;
-            vm->tam_pilha++;
+            vm->pilha[vm->tam_pilha++] = imediato;
             break;
         case INST_POP:
             imediato =  vm->pilha[vm->tam_pilha - 1];
             vm->tam_pilha--;
-            printf("Pop: ");
-            printf("0x%016lx\n", imediato);
+            break;
+        case INST_ADDU:
+            ;
+            operando2 = vm->pilha[--vm->tam_pilha];
+            operando1 = vm->pilha[--vm->tam_pilha];
+            vm->pilha[vm->tam_pilha++] = (operando1 + operando2);
+            break;
+        case INST_SUBU:
+            ;
+            operando2 = vm->pilha[--vm->tam_pilha];
+            operando1 = vm->pilha[--vm->tam_pilha];
+            vm->pilha[vm->tam_pilha++] = (operando1 - operando2);
+            break;
+        case INST_MULU:
+            ;
+            operando2 = vm->pilha[--vm->tam_pilha];
+            operando1 = vm->pilha[--vm->tam_pilha];
+            vm->pilha[vm->tam_pilha++] = (operando1 * operando2);
+            break;
+        case INST_DIVU:
+            ;
+            operando2 = vm->pilha[--vm->tam_pilha];
+            operando1 = vm->pilha[--vm->tam_pilha];
+            vm->pilha[vm->tam_pilha++] = (operando1 / operando2);
+            break;
+        case INST_REMU:
+            ;
+            operando2 = vm->pilha[--vm->tam_pilha];
+            operando1 = vm->pilha[--vm->tam_pilha];
+            vm->pilha[vm->tam_pilha++] = (operando1 % operando2);
             break;
         default:
-            printf("0x%02x\n", inst);
+            printf("Instrução desconhecida: 0x%02x\n", inst);
             break;
 
     }
@@ -63,7 +105,14 @@ int main() {
         .pc = 0,
         .pilha = {0},
         .tam_pilha = 0,
-        .programa = {0x02, 0x1F, 0x00, 0xFF, 0xEE, 0xDD, 0x20, 0xCC, 0x88, 0x03, 0x01},
+        .programa = {
+            INST_PUSH, TESTE_PILHA,
+            INST_POP,
+            INST_PUSH, X_7_0, 0xF4,
+            INST_PUSH, X_7_0, 0x24,
+            INST_REMU,
+            INST_HALT
+        },
         .tam_programa = 3,
         .memoria = {0},
         .halt = 0
