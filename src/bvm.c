61e9fa2fd561a5e088f4209d74a2ca6448c01b76
Florencio Natan <florencionatan@gmail.com>
2024-05-07 16:52:16 -0300

    Adiciona as instruções para operações para inteiros com sinal e float

--- src/bvm.c
+++ src/bvm.c
@@ -25,6 +25,49 @@ void operacao_binaria_u(bvm *vm, char operador) {
     }
 }
 
+void operacao_binaria_i(bvm *vm, char operador) {
+    int64_t operando2 = vm->pilha[--vm->tam_pilha];
+    int64_t operando1 = vm->pilha[--vm->tam_pilha];
+
+    switch (operador) {
+        case '+':
+            vm->pilha[vm->tam_pilha++] = (operando1 + operando2);
+            break;
+        case '-':
+            vm->pilha[vm->tam_pilha++] = (operando1 - operando2);
+            break;
+        case '*':
+            vm->pilha[vm->tam_pilha++] = (operando1 * operando2);
+            break;
+        case '/':
+            vm->pilha[vm->tam_pilha++] = (operando1 / operando2);
+            break;
+        case '%':
+            vm->pilha[vm->tam_pilha++] = (operando1 % operando2);
+            break;
+    }
+}
+
+void operacao_binaria_f(bvm *vm, char operador) {
+    double operando2 = vm->pilha[--vm->tam_pilha];
+    double operando1 = vm->pilha[--vm->tam_pilha];
+
+    switch (operador) {
+        case '+':
+            vm->pilha[vm->tam_pilha++] = (operando1 + operando2);
+            break;
+        case '-':
+            vm->pilha[vm->tam_pilha++] = (operando1 - operando2);
+            break;
+        case '*':
+            vm->pilha[vm->tam_pilha++] = (operando1 * operando2);
+            break;
+        case '/':
+            vm->pilha[vm->tam_pilha++] = (operando1 / operando2);
+            break;
+    }
+}
+
 void processar_instrucoes(bvm *vm) {
     uint8_t inst = vm->programa[vm->pc++];
     uint64_t imediato = 0;
@@ -46,6 +89,7 @@ void processar_instrucoes(bvm *vm) {
             imediato =  vm->pilha[vm->tam_pilha - 1];
             vm->tam_pilha--;
             break;
+
         case INST_ADDU:
             operacao_binaria_u(vm, '+');
             break;
@@ -61,6 +105,36 @@ void processar_instrucoes(bvm *vm) {
         case INST_REMU:
             operacao_binaria_u(vm, '%');
             break;
+
+        case INST_ADD:
+            operacao_binaria_i(vm, '+');
+            break;
+        case INST_SUB:
+            operacao_binaria_i(vm, '-');
+            break;
+        case INST_MUL:
+            operacao_binaria_i(vm, '*');
+            break;
+        case INST_DIV:
+            operacao_binaria_i(vm, '/');
+            break;
+        case INST_REM:
+            operacao_binaria_i(vm, '%');
+            break;
+
+        case INST_ADDF:
+            operacao_binaria_f(vm, '+');
+            break;
+        case INST_SUBF:
+            operacao_binaria_f(vm, '-');
+            break;
+        case INST_MULF:
+            operacao_binaria_f(vm, '*');
+            break;
+        case INST_DIVF:
+            operacao_binaria_f(vm, '/');
+            break;
+
         default:
             printf("Instrução desconhecida: 0x%02x\n", inst);
             break;
--- src/instrucoes.h
+++ src/instrucoes.h
@@ -12,4 +12,15 @@
 #define INST_DIVU 0x07
 #define INST_REMU 0x08
 
+#define INST_ADD 0x09
+#define INST_SUB 0x0A
+#define INST_MUL 0x0B
+#define INST_DIV 0x0C
+#define INST_REM 0x0D
+
+#define INST_ADDF 0x0E
+#define INST_SUBF 0x0F
+#define INST_MULF 0x10
+#define INST_DIVF 0x11
+
 #endif /* INSTRUCOES_H */
